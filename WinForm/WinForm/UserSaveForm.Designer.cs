8e8e09ab1226439ce2cb9de9d164d1e730c93f1f
HIDxxx1 <gittogitto35@gmail.com>
2021-03-15 23:05:16 +0900

    コントロール初期

---
+++ WinForm/WinForm/UserSaveForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace WinForm
+{
+    partial class UserSaveForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDlabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.MailCheckBox = new System.Windows.Forms.CheckBox();
+            this.MailAddressLabel = new System.Windows.Forms.Label();
+            this.MailAddressTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FreeRadioButton = new System.Windows.Forms.RadioButton();
+            this.BusinessRadioButton = new System.Windows.Forms.RadioButton();
+            this.NateLabel = new System.Windows.Forms.Label();
+            this.EnableComboBox = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // IDlabel
+            // 
+            this.IDlabel.AutoSize = true;
+            this.IDlabel.Location = new System.Drawing.Point(23, 29);
+            this.IDlabel.Name = "IDlabel";
+            this.IDlabel.Size = new System.Drawing.Size(32, 24);
+            this.IDlabel.TabIndex = 0;
+            this.IDlabel.Text = "ID";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(119, 26);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(201, 31);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // MailCheckBox
+            // 
+            this.MailCheckBox.AutoSize = true;
+            this.MailCheckBox.Location = new System.Drawing.Point(27, 80);
+            this.MailCheckBox.Name = "MailCheckBox";
+            this.MailCheckBox.Size = new System.Drawing.Size(203, 28);
+            this.MailCheckBox.TabIndex = 2;
+            this.MailCheckBox.Text = "メールを送信する";
+            this.MailCheckBox.UseVisualStyleBackColor = true;
+            this.MailCheckBox.CheckedChanged += new System.EventHandler(this.MailCheckBox_CheckedChanged);
+            // 
+            // MailAddressLabel
+            // 
+            this.MailAddressLabel.AutoSize = true;
+            this.MailAddressLabel.Location = new System.Drawing.Point(23, 151);
+            this.MailAddressLabel.Name = "MailAddressLabel";
+            this.MailAddressLabel.Size = new System.Drawing.Size(138, 24);
+            this.MailAddressLabel.TabIndex = 3;
+            this.MailAddressLabel.Text = "メールアドレス";
+            // 
+            // MailAddressTextBox
+            // 
+            this.MailAddressTextBox.Location = new System.Drawing.Point(190, 144);
+            this.MailAddressTextBox.Name = "MailAddressTextBox";
+            this.MailAddressTextBox.Size = new System.Drawing.Size(201, 31);
+            this.MailAddressTextBox.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BusinessRadioButton);
+            this.groupBox1.Controls.Add(this.FreeRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(30, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "プラン";
+            // 
+            // FreeRadioButton
+            // 
+            this.FreeRadioButton.AutoSize = true;
+            this.FreeRadioButton.Checked = true;
+            this.FreeRadioButton.Location = new System.Drawing.Point(7, 42);
+            this.FreeRadioButton.Name = "FreeRadioButton";
+            this.FreeRadioButton.Size = new System.Drawing.Size(89, 28);
+            this.FreeRadioButton.TabIndex = 0;
+            this.FreeRadioButton.TabStop = true;
+            this.FreeRadioButton.Text = "無料";
+            this.FreeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // BusinessRadioButton
+            // 
+            this.BusinessRadioButton.AutoSize = true;
+            this.BusinessRadioButton.Location = new System.Drawing.Point(214, 42);
+            this.BusinessRadioButton.Name = "BusinessRadioButton";
+            this.BusinessRadioButton.Size = new System.Drawing.Size(116, 28);
+            this.BusinessRadioButton.TabIndex = 1;
+            this.BusinessRadioButton.Text = "ビジネス";
+            this.BusinessRadioButton.UseVisualStyleBackColor = true;
+            this.BusinessRadioButton.CheckedChanged += new System.EventHandler(this.BusinessRadioButton_CheckedChanged);
+            // 
+            // NateLabel
+            // 
+            this.NateLabel.AutoSize = true;
+            this.NateLabel.Location = new System.Drawing.Point(37, 327);
+            this.NateLabel.Name = "NateLabel";
+            this.NateLabel.Size = new System.Drawing.Size(183, 24);
+            this.NateLabel.TabIndex = 6;
+            this.NateLabel.Text = "無制限で使えます";
+            // 
+            // EnableComboBox
+            // 
+            this.EnableComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EnableComboBox.FormattingEnabled = true;
+            this.EnableComboBox.Items.AddRange(new object[] {
+            "有効",
+            "無効"});
+            this.EnableComboBox.Location = new System.Drawing.Point(37, 389);
+            this.EnableComboBox.Name = "EnableComboBox";
+            this.EnableComboBox.Size = new System.Drawing.Size(268, 32);
+            this.EnableComboBox.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 489);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(769, 78);
+            this.panel1.TabIndex = 8;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SaveButton.Location = new System.Drawing.Point(0, 0);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(153, 78);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "保存";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CloseButton.Location = new System.Drawing.Point(153, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(155, 78);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "閉じる";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 567);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(769, 42);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(240, 32);
+            this.StatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // UserSaveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 609);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.EnableComboBox);
+            this.Controls.Add(this.NateLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MailAddressTextBox);
+            this.Controls.Add(this.MailAddressLabel);
+            this.Controls.Add(this.MailCheckBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.IDlabel);
+            this.Name = "UserSaveForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ユーザー登録";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IDlabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.CheckBox MailCheckBox;
+        private System.Windows.Forms.Label MailAddressLabel;
+        private System.Windows.Forms.TextBox MailAddressTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton BusinessRadioButton;
+        private System.Windows.Forms.RadioButton FreeRadioButton;
+        private System.Windows.Forms.Label NateLabel;
+        private System.Windows.Forms.ComboBox EnableComboBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+    }
+}
+
---
+++ WinForm/WinForm/UserSaveForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class UserSaveForm : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public UserSaveForm()
+        {
+            InitializeComponent();
+            StatusLabel.Text = "必要事項を入力お願いいたします。";
+
+            EnableComboBox.SelectedIndex = 0;
+            //EnableComboBox.Items.Add("有効");
+            //EnableComboBox.Items.Add("無効");
+            //CheckBoxの初期表示
+            SetMailAddressTextBox();
+            //ラベルの初期表示
+            SetNateLabel();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MailCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            SetMailAddressTextBox();
+        }
+
+        /// <summary>
+        /// CheckBoxの初期表示
+        /// </summary>
+        private void SetMailAddressTextBox()
+        {
+            MailAddressTextBox.Enabled = MailCheckBox.Checked;
+            MailAddressLabel.Enabled = MailCheckBox.Checked;
+
+        }
+
+        private void BusinessRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            SetNateLabel();
+        }
+
+        /// <summary>
+        /// ラベルのセット
+        /// </summary>
+        public void SetNateLabel()
+        {
+            //if (BusinessRadioButton.Checked)
+            //{
+            //    NateLabel.Visible = true;
+            //}
+            //else
+            //{
+            //    NateLabel.Visible = false;
+
+            //}]
+
+            //上記は冗長な実装
+            NateLabel.Visible = BusinessRadioButton.Checked;
+
+        }
+
+        /// <summary>
+        /// 保存ボタン保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+          DialogResult dialog =  MessageBox.Show(
+                "保存しますか？",
+                "確認", 
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if(dialog == DialogResult.OK)
+            {
+                StatusLabel.Text = "保存しました。";
+            }
+            else
+            {
+                StatusLabel.Text = "キャンセルしました。";
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
